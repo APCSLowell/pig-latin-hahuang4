bcb012d0e6473c041e7d094347a8bbfc828473d3
hahuang4 <142834501+hahuang4@users.noreply.github.com>
2024-01-26 22:45:21 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -31,18 +31,46 @@ public class PigLatin {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
 	    // your code goes here
-        return -1;
+        for (int i = 0; i < sWord.length(); i++) 
+    {
+        if (sWord.substring(i, i + 1).equals("a") || sWord.substring(i, i + 1).equals("e") || 
+            sWord.substring(i, i + 1).equals("i") || sWord.substring(i, i + 1).equals("o") || 
+            sWord.substring(i, i + 1).equals("u")) 
+        {
+            return i;
+        }
+    }
+    return -1;
     }
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
         // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
+	    int firstVowelIndex = findFirstVowel(sWord);
+    
+    if (firstVowelIndex == -1)
+    {
+        return sWord + "ay";
+    }
+    else if (firstVowelIndex == 0)
+    {
+        return sWord + "way";
+    }
+    else 
+    {
+        String beginningConsonants = sWord.substring(0, firstVowelIndex);
+        String restOfWord = sWord.substring(firstVowelIndex);
+        
+        // For words starting with "qu"
+        if (beginningConsonants.toLowerCase().equals("qu")) 
+        {
+            return restOfWord + beginningConsonants + "ay";
+        } 
+        else 
+        {
+            return restOfWord + beginningConsonants + "ay";
+        }
+    }
     }
 }//end PigLatin class
